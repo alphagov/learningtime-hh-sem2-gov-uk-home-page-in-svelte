1a527b99eaffd39c6e9f6845b26c22858b56aa11
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-07-20 17:32:53 +0100

    Add GovernmentActivity component:
            - Update main content page to import GovernmentActivity component
            - Add sub-component (BigNumberContainer)

---
+++ src/lib/components/home_page_sections/GovernmentActivity/BigNumberContainer.svelte
@@ -0,0 +1,31 @@
+
+<div>
+  <a href="https://www.google.co.uk/">
+  </a>
+</div>
+
+<style>
+
+div {
+  /* fit content inside the div so whole area is a tag hyperlink */ 
+  width: fit-content;
+}
+
+a {
+  font-weight: bold;
+  color: #1d70b8;
+  text-decoration: none;
+}
+
+a::before {
+  content: '24';
+  font-size: 5rem;
+  display: block;
+}
+
+a::after {
+  content: 'Ministerial Departments';
+}
+
+</style>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import HomepageHeader from '$lib/components/home_page_sections/HomepageHeader/HomepageHeader.svelte';
 	import HomepageLinksAndSearch from '$lib/components/home_page_sections/HomepageLinksAndSearch/HomepageLinksAndSearch.svelte';
 	import ServicesAndInfo from '$lib/components/home_page_sections/ServicesAndInfo/ServicesAndInfo.svelte';
-	// import GovernmentActivity from '$lib/components/home_page_sections/GovernmentActivity/GovernmentActivity.svelte';
+	import GovernmentActivity from '$lib/components/home_page_sections/GovernmentActivity/GovernmentActivity.svelte';
 	// import FeaturedServices from '$lib/components/home_page_sections/FeaturedServices/FeaturedServices.svelte';
 	// import MoreServices from '$lib/components/home_page_sections/MoreServices/MoreServices.svelte';
 	// import PromptContent from '$lib/components/home_page_sections/PromptContent/PromptContent.svelte';
@@ -12,8 +12,8 @@
 	<HomepageHeader />
 	<HomepageLinksAndSearch />
 	<ServicesAndInfo />
-	<!-- <GovernmentActivity />
-	<FeaturedServices />
+	<GovernmentActivity />
+	<!-- <FeaturedServices />
 	<MoreServices />
 	<PromptContent /> -->
 </main>
