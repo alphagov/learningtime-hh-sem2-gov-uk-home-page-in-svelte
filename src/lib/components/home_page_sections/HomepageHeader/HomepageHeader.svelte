7481b9f9595f82e2332f470ac547ca70c7df4d63
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-07-20 17:01:15 +0100

    Add initial svelte files:
    - Create svelte layout page with NavBar
    - Create main content page
    - Add HomepageHeader component
    - Add required sub-components (PageContentWrapper, WidthContainer)

---
+++ src/lib/components/home_page_sections/HomepageHeader/HomepageHeader.svelte
@@ -0,0 +1,18 @@
+<script>
+	import WidthContainer from '../../shared/WidthContainer.svelte';
+</script>
+
+<header>
+	<WidthContainer>
+		<h1>Welcome to GOV.UK</h1>
+		<p>The best place to find government service and information</p>
+		<p>Simpler, clearer, faster</p>
+	</WidthContainer>
+</header>
+
+<style>
+	header {
+		color: var(--govuk-white);
+		background-color: var(--govuk-blue);
+	}
+</style>
---
+++ src/lib/components/shared/WidthContainer.svelte
@@ -0,0 +1,10 @@
+<div>
+	<slot />
+</div>
+
+<style>
+	div {
+		margin-right: 15px;
+		margin-left: 15px;
+	}
+</style>
