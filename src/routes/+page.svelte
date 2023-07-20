0cba59479fc98b58350fc72b37eeabab62bfa3a0
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-07-20 17:35:35 +0100

    Add FeaturedServices component:
            - Update main content page to import FeaturedServices component
            - Add sub-component (FeaturedServiceCard)
    	- Add image assets for FeaturedServices

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 	import HomepageLinksAndSearch from '$lib/components/home_page_sections/HomepageLinksAndSearch/HomepageLinksAndSearch.svelte';
 	import ServicesAndInfo from '$lib/components/home_page_sections/ServicesAndInfo/ServicesAndInfo.svelte';
 	import GovernmentActivity from '$lib/components/home_page_sections/GovernmentActivity/GovernmentActivity.svelte';
-	// import FeaturedServices from '$lib/components/home_page_sections/FeaturedServices/FeaturedServices.svelte';
+	import FeaturedServices from '$lib/components/home_page_sections/FeaturedServices/FeaturedServices.svelte';
 	// import MoreServices from '$lib/components/home_page_sections/MoreServices/MoreServices.svelte';
 	// import PromptContent from '$lib/components/home_page_sections/PromptContent/PromptContent.svelte';
 </script>
@@ -13,7 +13,7 @@
 	<HomepageLinksAndSearch />
 	<ServicesAndInfo />
 	<GovernmentActivity />
-	<!-- <FeaturedServices />
-	<MoreServices />
+	<FeaturedServices />
+	<!-- <MoreServices />
 	<PromptContent /> -->
 </main>
