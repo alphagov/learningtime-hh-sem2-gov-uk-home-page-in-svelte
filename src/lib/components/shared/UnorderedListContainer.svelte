78e3124345fa497eb56ef52284a04ed0c8bd626e
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-08-10 10:23:11 +0100

    Add margin and padding for Government Activity section

--- src/lib/components/shared/UnorderedListContainer.svelte
+++ src/lib/components/shared/UnorderedListContainer.svelte
@@ -1,19 +1,17 @@
 <div class="unordered-list-container">
-  <slot />
+	<slot />
 </div>
 
 <style>
+	.unordered-list-container {
+		grid-template-columns: 1fr 1fr;
+	}
 
-.unordered-list-container {
-  grid-template-columns: 1fr 1fr;
-}
-
-@media screen and (min-width: 900px) {
-  .unordered-list-container {
-    display: grid;
-    column-gap: 15px;
-    grid-template-columns: 2fr 1fr;
-  }
-}
-
+	@media screen and (min-width: 900px) {
+		.unordered-list-container {
+			display: grid;
+			column-gap: 15px;
+			grid-template-columns: 2fr 1fr;
+		}
+	}
 </style>
